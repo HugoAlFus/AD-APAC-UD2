c16e38832451fe9da7389d3aa765849bc6cf409d
HugoAlm-DAM <hugoalfus@gmail.com>
2024-10-27 06:50:51 +0100

    Dao documentados

--- src/main/java/es/cheste/dao/ClienteDAO.java
+++ src/main/java/es/cheste/dao/ClienteDAO.java
@@ -1,22 +1,58 @@
 package es.cheste.dao;
 
-import es.cheste.entidad.Chef;
 import es.cheste.entidad.Cliente;
 import es.cheste.utilidad.DAOException;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Interface ClienteDAO
+ * <p>
+ * Proporciona métodos para realizar operaciones CRUD en la entidad Cliente.
+ *
+ * @author Hugo Almodóvar Fuster
+ * @version 1.0
+ */
 public interface ClienteDAO {
 
+    /**
+     * Inserta un nuevo cliente en la base de datos.
+     *
+     * @param cliente El objeto Cliente a insertar.
+     * @throws DAOException Si ocurre un error durante la inserción.
+     */
     void insertar(Cliente cliente) throws DAOException;
 
+    /**
+     * Obtiene un cliente por su ID.
+     *
+     * @param idCliente El ID del cliente a obtener.
+     * @return El objeto Cliente correspondiente al ID proporcionado.
+     * @throws DAOException Si ocurre un error durante la obtención.
+     */
     Cliente obtenerPorID(int idCliente) throws DAOException;
 
+    /**
+     * Obtiene una lista de todos los clientes.
+     *
+     * @return Una lista de objetos Cliente.
+     * @throws DAOException Si ocurre un error durante la obtención.
+     */
     List<Cliente> obtenerTodos() throws DAOException;
 
+    /**
+     * Actualiza la información de un cliente existente.
+     *
+     * @param cliente El objeto Cliente con la información actualizada.
+     * @throws DAOException Si ocurre un error durante la actualización.
+     */
     void actualizar(Cliente cliente) throws DAOException;
 
+    /**
+     * Elimina un cliente por su ID.
+     *
+     * @param idCliente El ID del cliente a eliminar.
+     * @throws DAOException Si ocurre un error durante la eliminación.
+     */
     void eliminar(int idCliente) throws DAOException;
 }
--- src/main/java/es/cheste/dao/MesaDAO.java
+++ src/main/java/es/cheste/dao/MesaDAO.java
@@ -3,20 +3,57 @@ package es.cheste.dao;
 import es.cheste.entidad.Mesa;
 import es.cheste.utilidad.DAOException;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Interface MesaDAO
+ * <p>
+ * Proporciona métodos para realizar operaciones CRUD en la entidad Mesa.
+ *
+ * @author Hugo Almodóvar Fuster
+ * @version 1.0
+ */
 public interface MesaDAO {
 
+    /**
+     * Inserta una nueva mesa en la base de datos.
+     *
+     * @param mesa El objeto Mesa a insertar.
+     * @throws DAOException Si ocurre un error durante la inserción.
+     */
     void insertar(Mesa mesa) throws DAOException;
 
+    /**
+     * Obtiene una mesa por su ID.
+     *
+     * @param idMesa El ID de la mesa a obtener.
+     * @return El objeto Mesa correspondiente al ID proporcionado.
+     * @throws DAOException Si ocurre un error durante la obtención.
+     */
     Mesa obtenerPorID(int idMesa) throws DAOException;
 
+    /**
+     * Obtiene una lista de todas las mesas.
+     *
+     * @return Una lista de objetos Mesa.
+     * @throws DAOException Si ocurre un error durante la obtención.
+     */
     List<Mesa> obtenerTodos() throws DAOException;
 
+    /**
+     * Actualiza la información de una mesa existente.
+     *
+     * @param mesa El objeto Mesa con la información actualizada.
+     * @throws DAOException Si ocurre un error durante la actualización.
+     */
     void actualizar(Mesa mesa) throws DAOException;
 
+    /**
+     * Elimina una mesa por su ID.
+     *
+     * @param idMesa El ID de la mesa a eliminar.
+     * @throws DAOException Si ocurre un error durante la eliminación.
+     */
     void eliminar(int idMesa) throws DAOException;
 
 }
